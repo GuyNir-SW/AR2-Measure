1bba90e8ed2fc2650ced937f263376a3158ede7d
Guy Nir <g.nir@f5.com>
2019-10-13 05:10:02 +0300

    AR common definitions

--- AR2-Measure/ViewController.swift
+++ AR2-Measure/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    //------------------------------
+    //MARK: Properties
+    //------------------------------
+    
+    // AR basic stuff
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+
+    
+    
+    //MARK: VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //------------------------------------
+        // AR basic stuff
+        //------------------------------------
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Debug options
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
+                                       ARSCNDebugOptions.showWorldOrigin]
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Detect planes
+        //self.configuration.planeDetection = .horizontal
+        
+        // Run the scene
+        sceneView.session.run(configuration)
+        
+        //------------------------------------
+        // Make sure required widgets are at front
+        //------------------------------------
+        
+        //view.bringSubviewToFront(...)
+        
     }
+    
+    
+    
+    //MARK: Additional methods
+    
 
 
 }
-
