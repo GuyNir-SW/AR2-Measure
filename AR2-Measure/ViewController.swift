9404f25d19d01a1a17377b67a2be12ebb2c12ba0
Guy Nir <g.nir@f5.com>
2019-10-13 05:52:35 +0300

    Add Gesture and few labels

--- AR2-Measure/ViewController.swift
+++ AR2-Measure/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
 
+    // Widgets
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
     
     
     //MARK: VIEW DID LOAD
@@ -46,6 +52,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
         
         //------------------------------------
+        // AR Gestures
+        //------------------------------------
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        //------------------------------------
         // Make sure required widgets are at front
         //------------------------------------
         
@@ -53,9 +66,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
+    //------------------------------
+    //MARK: AR Gesture methods
+    //------------------------------
     
+    @objc func handleTapGesture(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        
+        // Get current frame, in order to tell orientation of camera
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        
+        // Calculate the location that will be infront of the camera (not sure this is right calculation)
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.z = -0.1
+        var modifiedMatrix = simd_mul(transform, translationMatrix)
+        
+        // Create a small sphere infront of the camera
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = modifiedMatrix
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+    }
     
+    //------------------------------
     //MARK: Additional methods
+    //------------------------------
+    
     
 
 
